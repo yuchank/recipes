89bec4b79be67a3904135ef6b52344ddb0a2099c
kwon.yuchan <kwon.yuchan@nbplus.kr>
2018-04-13 17:03:54 +0900

    propTypes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class Summary extends React.Component {
+  static propTypes = {
+    ingredients: PropTypes.number.isRequired,
+    steps: PropTypes.number.isRequired,
+    title: PropTypes.string
+  };
+
+  static defaultProps = {
+    ingredients: 0,
+    steps: 0,
+    title: '[recipe]'
+  };
+
+  render() {
+    const { ingredients, steps, title } = this.props
+
+    return (
+      <div className="summary">
+        <h1>{ title }</h1>
+        <p>
+          <span>{ ingredients } Ingredients</span> | <span>{ steps } Steps</span>
+        </p>  
+      </div>  
+    );
+  }
+}
+
+// ReactDOM.render(<Summary title="Peanut Butter and Jelly" ingredients="peanut butter, jelly, bread" steps="spread peanut butter and jelly between bread" />, document.getElementById('root'));
+ReactDOM.render(<Summary />, document.getElementById('root'));
