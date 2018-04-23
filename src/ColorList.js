a5bb178e905cd0fef974c4dd861b2267a5617dbf
kwon.yuchan <kwon.yuchan@nbplus.kr>
2018-04-23 16:44:50 +0900

    typo

--- src/ColorList.js
+++ src/ColorList.js
@@ -6,7 +6,7 @@ import './stylesheets/ColorList.scss';
 
 const ColorList = ({ colors = [], onRate = f => f, onRemove = f => f }) =>
   <div className="color-list">
-    {(colors.length === 0) ? <p>No Colors Listed. (Add a Color)</p> : colors.map(color => <Color key={ color.id } {...color} onRate={ (rating) =>onRate(color.id, rating) } onRemove={ () => onRemove(color.id) }/>)}
+    {(colors.length === 0) ? <p>No Colors Listed. (Add a Color)</p> : colors.map(color => <Color key={ color.id } {...color} onRate={ (rating) => onRate(color.id, rating) } onRemove={ () => onRemove(color.id) }/>)}
   </div>;
 
 ColorList.propTypes = {
