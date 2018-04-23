f096022d95a00e898baa23b61f4840a0e97d5211
kwon.yuchan <kwon.yuchan@nbplus.kr>
2018-04-23 14:52:22 +0900

    scss

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
-import './index.css';
+import './index.scss';
 
 import { Component } from 'react';
 
