31ad58f6bf6cb3c92e23f5bd441aaa26a0a01b6a
kwon.yuchan <kwon.yuchan@nbplus.kr>
2018-04-23 16:39:24 +0900

    pass back

--- src/ColorList.js
+++ src/ColorList.js
@@ -4,9 +4,9 @@ import PropTypes from 'prop-types';
 import Color from './Color';
 import './stylesheets/ColorList.scss';
 
-const ColorList = ({ colors = [] }) =>
+const ColorList = ({ colors = [], onRate = f => f, onRemove = f => f }) =>
   <div className="color-list">
-    {(colors.length === 0) ? <p>No Colors Listed. (Add a Color)</p> : colors.map(color => <Color key={ color.id } {...color} />)}
+    {(colors.length === 0) ? <p>No Colors Listed. (Add a Color)</p> : colors.map(color => <Color key={ color.id } {...color} onRate={ (rating) =>onRate(color.id, rating) } onRemove={ () => onRemove(color.id) }/>)}
   </div>;
 
 ColorList.propTypes = {
