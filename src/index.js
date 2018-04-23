086a19f9a8bbf8cf744b131cc3205ee425c12de0
kwon.yuchan <kwon.yuchan@nbplus.kr>
2018-04-23 14:28:48 +0900

    component tree

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import './index.css';
 
+import { Component } from 'react';
+
 class Summary extends React.Component {
   static propTypes = {
     ingredients: PropTypes.number.isRequired,
@@ -119,3 +121,79 @@ class StarRating extends React.Component {
 }
 
 ReactDOM.render(<StarRating />, document.getElementById('star'));
+
+const StarRatingPresentational = ({ starsSelected = 0, totalStars = 5, onRate = f => f }) => 
+  <div className="star-rating">
+    {[...Array(totalStars)].map((n, i) => <Star key={ i } selected={ i < starsSelected } onClick={ () => onRate(i + 1) } />)}
+    <p>{ starsSelected } of { totalStars } stars</p>
+  </div>;
+
+StarRatingPresentational.propTypes = {
+  starsSelected: PropTypes.number,
+  totalStars: PropTypes.number,
+  onRate: PropTypes.func
+};
+
+const ColorList = ({ colors = [] }) =>
+  <div className="color-list">
+    {(colors.length === 0) ? <p>No Colors Listed. (Add a Color)</p> : colors.map(color => <Color key={ color.id } {...color} />)}
+  </div>;
+
+ColorList.propTypes = {
+  colors: PropTypes.array
+};
+
+const Color = ({ title, color, rating = 0 }) =>
+  <section className="color">
+    <h1>{ title }</h1>
+    <div className="color" style={{ backgroundColor: color }}></div>
+    <div>
+      <StarRatingPresentational starsSelected={ rating } />
+    </div>
+  </section>;
+
+Color.propTypes = {
+  title: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
+  rating: PropTypes.number
+};
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      colors: [
+        {
+          'id': '0175d1f0-a8c6-41bf-8d02-df5734d829a4',
+          'title': 'ocean at dusk',
+          'color': '#00c4e2',
+          'rating': 5
+        },
+        {
+          'id': '83c7ba2f-7392-4d7d-9e23-35adbe186046',
+          'title': 'lawn',
+          'color': '#26ac56',
+          'rating': 3
+        },
+        {
+          'id': 'a11e3995-b0bd-4d58-8c48-5e49ae7f7f23',
+          'title': 'bright red',
+          'color': '#ff0000',
+          'rating': 0
+        }
+      ]
+    }
+  }
+
+  render() {
+    const { colors } = this.state;
+    return (
+      <div className="app">
+        <AddColorForm />
+        <ColorList colors={ colors } />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
